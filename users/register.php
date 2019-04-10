829a9001a2de87728b4417409dce31def907ff64
Luka1468 <47037253+Luka1468@users.noreply.github.com>
2019-04-09 20:55:32 -0400

    adding header and comments

--- users/register.php
+++ users/register.php
@@ -5,15 +5,15 @@ require '../php/connect.php';
 $myPDO = new PDO('mysql:host=localhost;dbname=sunjingw_week11', $user, $passwd);
 $myPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-
+//gets users username, passwrod/email
 try {
 
     $name = $_POST['username'];
     $pass = $_POST['password'];
     $email = $_POST['email'];
-
+//adds the information into the table
 $sql = "INSERT INTO users (username, email, password) VALUES ('$name', '$email', '$pass')";
-
+//if it is added succesfully it sends it into the server and gives you a registration successfull
 $myPDO->exec($sql);
     echo "Registration successful.";
 
