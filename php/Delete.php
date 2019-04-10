5c255e3b0037015d16a728c2b89507d68e4e2b9e
Luka1468 <47037253+Luka1468@users.noreply.github.com>
2019-04-09 21:41:20 -0400

    create the headers and comments

--- php/Delete.php
+++ php/Delete.php
@@ -1,4 +1,5 @@
 <?php
+//this page is activated when the delete button is pressed underneath the post
 session_start();
 if(isset($_SESSION['user'])) {
 
@@ -15,7 +16,8 @@ if(isset($_SESSION['user'])) {
 
         $myPDO = new PDO('mysql:host=localhost;dbname=sunjingw_week11', $user, $passwd);
         $myPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+        
+//gets the id and then deletes the id from the specific row
         $sql = "DELETE FROM entries WHERE id='$id'";
         $myPDO->exec($sql);
         echo "Deleting entry...";
