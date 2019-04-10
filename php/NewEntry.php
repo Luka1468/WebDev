f154e8779255366a35f67a80abd0e2d4a1d63e67
Luka1468 <47037253+Luka1468@users.noreply.github.com>
2019-04-09 21:57:29 -0400

    headers and comments

--- php/NewEntry.php
+++ php/NewEntry.php
@@ -1,3 +1,7 @@
+<!--
+This page is for sending the information to the server
+-->
+
 <?php
 session_start();
 if (isset($_SESSION['user'])) {
@@ -21,7 +25,7 @@ if (isset($_SESSION['user'])) {
         $myPDO = new PDO('mysql:host=localhost;dbname=sunjingw_week11', $user, $passwd);
         $myPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-
+//inserting the items into the appropriate catagories
         $sql = "INSERT INTO `entries` (`author`, `title`, `summary`, `image`, `body`, `created`) 
 VALUES ('$author', '$title', '$summary', '$image', '$body', '$date')";
 
