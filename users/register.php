35298b45f1fa587f1d12b1276833a2466d6d9695
Luka1468 <47037253+Luka1468@users.noreply.github.com>
2019-04-09 21:38:02 -0400

    updating headers and comments

--- users/register.php
+++ users/register.php
@@ -1,19 +1,25 @@
 <?php
+//this page sends the information to the server
+
+
+
+
 
 require '../php/connect.php';
 
 $myPDO = new PDO('mysql:host=localhost;dbname=sunjingw_week11', $user, $passwd);
 $myPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-
+//gets users username, password/email
 try {
 
     $name = $_POST['username'];
     $pass = $_POST['password'];
     $email = $_POST['email'];
-
+//adds the information into the table
 $sql = "INSERT INTO users (username, email, password) VALUES ('$name', '$email', '$pass')";
-
+    
+//if it is added successfully it sends it into the server and gives you a registration successful
 $myPDO->exec($sql);
     echo "Registration successful.";
 
